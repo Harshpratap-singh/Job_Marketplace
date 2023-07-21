ff35ffeef68feebb94c72cd1b206744088252005
Harshpratap-singh <107066198+Harshpratap-singh@users.noreply.github.com>
2023-07-22 00:09:59 +0530

    update

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://job-marketplace.vercel.app/api/",
+  baseURL: "https://job-marketplace.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
